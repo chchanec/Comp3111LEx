ce1410ab0866a7445c40ef06087b15d5f759973d
CHAN, Chun Hin <chchanec@ust.hk>
2023-09-11 09:12:46 +0800

    Deleted 4th print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -12,6 +12,5 @@ public class mainApp1 {
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+ myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
-        System.out.println("Thank you for using me!");
     }
 }
